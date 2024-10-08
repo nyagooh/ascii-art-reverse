e559ec73853652ef053ad24f3c56533327fad8bd
nyagooh <annmaina614@gmail.com>
2024-10-08 11:40:25 +0300

    Add a function that extracts width of an ascii and matches it to a map to extract the letter

--- reverse/artextractor.go
+++ reverse/artextractor.go
@@ -13,3 +13,17 @@ func ProcessReverseFileLines(fileContent string) []string {
 
 	return processedLines
 }
+
+func ExtractAsciiArt(board [][]string, startIndex, width int) string {
+	rows := len(board)
+	cols := len(board[0])
+	result := ""
+	for i := 0; i < rows; i++ {
+		for j := startIndex; j < startIndex+width; j++ {
+			if j < cols {
+				result += board[i][j]
+			}
+		}
+	}
+	return result
+}
